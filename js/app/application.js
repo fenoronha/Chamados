e881d1353e67dda7493d182f220f9338e713820b
Fernando Noronha <fernando.noronha@ameris.com.br>
2013-08-17 12:35:41 -0300

    configuracao do plugin textJS (plugin requireJS) e underscoreJS para o gerenciamento de templates HTML

--- js/app/application.js
+++ js/app/application.js
@@ -1,2 +1,7 @@
-$('body').append('<h1>Chamados</h1>');
+require(['text!views/test/test.html'], function(html){
+	
+	var test_html = _.template(html);
+
+	$('body').append(test_html({title:'opa'}));
+});
 
--- js/app/config.js
+++ js/app/config.js
@@ -2,14 +2,16 @@
 
 	require.config({
 		paths: {
-			'jquery': 'js/libs/jquery-1.10.2'
+			'text': 'js/libs/text',
+			'jquery': 'js/libs/jquery-1.10.2',
+			'underscore': 'js/libs/underscore'
 		},
 		baseUrl: 'http://delta.chamados:8888/',
 		urlArgs: '_'+ Math.random() // evitar o cache dos arquivos carregados
 	});
 
-	require(['require','jquery'], function(require, $){
-		var dependencies = [
+	require(['require','text','underscore','jquery'], function(require, text, _, $){
+		var dependencies = [	
 								'js/app/application'
 							];
 
